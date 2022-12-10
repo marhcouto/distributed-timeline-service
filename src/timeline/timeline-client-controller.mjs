82de0f582c90d7dcd4362a72a69f8644202238e7
Francisco Pinto de Oliveira <up201907361@edu.fe.up.pt>
2022-12-10 22:04:12 +0000

    Solved bug

--- src/timeline/timeline-client-controller.mjs
+++ src/timeline/timeline-client-controller.mjs
@@ -11,7 +11,7 @@ export class TimelineClientController {
     app.post('/api/following/:id', this._followUser.bind(this));
     app.post('/api/timeline', this._postNewMessage.bind(this));
     app.delete('/api/following/:id', this._unfollowUser.bind(this));
-    app.get('/api/timeline', this._getTimeline.bind(this));
+    app.get('/api/timeline', this._getUserFeed.bind(this));
     app.get('/api/timeline/sync', this._syncTimeline.bind(this));
     app.get('/api/timeline/:id', this._getTimelineForUser.bind(this));
   }
